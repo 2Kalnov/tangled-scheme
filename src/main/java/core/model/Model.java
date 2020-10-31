ff25c87791cbf5a0b2dcd0a87be575d9e366f5fc
nikitakalnov <jdmcorolla21@gmail.com>
2020-10-31 13:40:28 +0400

    Использование фабрики клубка

--- src/main/java/core/model/Model.java
+++ src/main/java/core/model/Model.java
@@ -12,6 +12,19 @@ import core.field.geometry.Point;
 import lombok.Getter;
 
 public class Model implements TangleStateListener {
+  private static final double FIELD_WIDTH = 200;
+  private static final double FIELD_HEIGHT = 200;
+
+  private TangleFactory tangleFactory;
+
+  public Model() {
+    this.tangleFactory = new FourCrossingsTangleFactory();
+  }
+
+  public Model(TangleFactory factory) {
+    this.tangleFactory = factory;
+  }
+
   @Override
   public void tangleStateChanged() {
     Tangle tangle = gameField.getTangle();
