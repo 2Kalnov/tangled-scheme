399c17c9848394ceedc54ae9ebda13d33c89d672
nikitakalnov <jdmcorolla21@gmail.com>
2020-10-31 13:21:26 +0400

    Изменены названия методов в интерфейсах слушателей

--- src/main/java/core/event/NodeListener.java
+++ src/main/java/core/event/NodeListener.java
@@ -1,5 +1,5 @@
 package core.event;
 
 public interface NodeListener {
-  void update(int nodeId);
+  void nodeChanged();
 }
--- src/main/java/core/event/TangleStateListener.java
+++ src/main/java/core/event/TangleStateListener.java
@@ -1,5 +1,5 @@
 package core.event;
 
 public interface TangleStateListener {
-  void update(boolean isTangled);
+  void tangleStateChanged();
 }
