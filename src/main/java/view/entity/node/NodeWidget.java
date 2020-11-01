09f29d9a61c85b7f8bc99bf418d2b9e72eb6314c
nikitakalnov <jdmcorolla21@gmail.com>
2020-11-01 19:32:29 +0400

    Отрисовка узла вместе с границами

--- src/main/java/view/entity/node/NodeWidget.java
+++ src/main/java/view/entity/node/NodeWidget.java
@@ -22,8 +22,10 @@ public class NodeWidget extends JPanel {
     this.color = color;
   }
 
-  public int getDiameter() {
-    return NODE_DIAMETER;
+  @Override
+  public Dimension getSize() {
+    int size = NODE_DIAMETER;
+    return new Dimension(size, size);
   }
 
   public core.field.geometry.Point getPosition() {
@@ -34,12 +36,27 @@ public class NodeWidget extends JPanel {
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     Graphics2D graphics2D = (Graphics2D)g;
+    graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-    double x = this.node.getPosition().getX();
-    double y = this.node.getPosition().getY();
+    int x = this.node.getPosition().getX();
+    int y = this.node.getPosition().getY();
 
-    Shape node = new Ellipse2D.Double(x, y, NODE_DIAMETER, NODE_DIAMETER);
-    graphics2D.fill(node);
+    // Граница узла
+    graphics2D.setColor(Color.BLACK);
+    graphics2D.fillOval(
+            0, 0,
+            NODE_DIAMETER,
+            NODE_DIAMETER
+    );
+
+    // Узел
+    graphics2D.setColor(this.color);
+    graphics2D.fillOval(
+            BORDER_SIZE / 2,
+            BORDER_SIZE / 2,
+            NODE_DIAMETER - BORDER_SIZE,
+            NODE_DIAMETER - BORDER_SIZE
+    );
   }
 
   @Override
