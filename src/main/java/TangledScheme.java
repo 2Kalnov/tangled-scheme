c4f6655bd88c32fc1a209478b9c1379923493028
nikitakalnov <jdmcorolla21@gmail.com>
2020-11-01 13:01:35 +0400

    Инициализация процесса игры

--- src/main/java/TangledScheme.java
+++ src/main/java/TangledScheme.java
@@ -4,13 +4,16 @@ import core.model.Model;
 import view.GamePanel;
 import view.field.FieldWidget;
 
+import javax.swing.*;
+
 public class TangledScheme {
   public static void main(String[] args) {
     TangleFactory factory = new FourCrossingsTangleFactory();
     Model model = new Model(factory);
-
+    model.start();
+    
     FieldWidget fieldWidget = new FieldWidget(model.getFieldWidth(), model.getFieldHeight());
 
-    GamePanel mainWindow = new GamePanel(model, fieldWidget);
+    SwingUtilities.invokeLater(() -> new GamePanel(model, fieldWidget));
   }
 }
