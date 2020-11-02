2b010dbe1fb0e5871fc40f27efd8902da711a501
nikitakalnov <jdmcorolla21@gmail.com>
2020-11-02 20:08:50 +0400

    Обновление виджетов нитей при изменении клубка

--- src/main/java/view/entity/tangle/TangleWidget.java
+++ src/main/java/view/entity/tangle/TangleWidget.java
@@ -1,15 +1,19 @@
 package view.entity.tangle;
 
+import core.entity.edge.Edge;
 import core.entity.tangle.Tangle;
+import core.event.TangleStateListener;
 import view.entity.edge.EdgeWidget;
+import view.entity.edge.EdgeWidgetFactory;
 import view.entity.node.NodeWidget;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
 
-public class TangleWidget extends JPanel {
+public class TangleWidget extends JPanel implements TangleStateListener {
   private Tangle tangle;
   private final Set<NodeWidget> nodes;
   private final Set<EdgeWidget> edges;
@@ -17,6 +21,9 @@ public class TangleWidget extends JPanel {
   TangleWidget(Set<NodeWidget> nodes, Set<EdgeWidget> edges, Tangle tangle) {
     this.nodes = nodes;
     this.edges = edges;
+    this.tangle = tangle;
+
+    tangle.addListener(this);
   }
 
   public Set<NodeWidget> getNodes() {
@@ -25,6 +32,16 @@ public class TangleWidget extends JPanel {
 
   public Set<EdgeWidget> getEdges() {
     return Collections.unmodifiableSet(edges);
+  }
+
+  @Override
+  public void tangleStateChanged() {
+    Map<Edge, Boolean> edgesEntanglement = tangle.getEdgesEntanglement();
+
+    for(EdgeWidget edge : edges) {
+      boolean isCrossed = edgesEntanglement.get(edge.getEdge());
+      EdgeWidgetFactory.colorize(edge, isCrossed);
+    }
   }
 
   @Override
