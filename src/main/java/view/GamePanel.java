946f1c380b6128f3e8013fc010a0326279799832
nikitakalnov <jdmcorolla21@gmail.com>
2020-11-02 22:51:59 +0400

    Подписка на узлы виджета клубка

--- src/main/java/view/GamePanel.java
+++ src/main/java/view/GamePanel.java
@@ -1,25 +1,39 @@
 package view;
 
+import core.entity.node.Node;
 import core.event.GameStateListener;
+import core.field.PositionOccupiedException;
+import core.field.PositionOutOfFieldException;
+import core.field.geometry.Point;
 import core.model.Model;
+import view.entity.node.NodeWidget;
 import view.entity.tangle.TangleWidget;
 import view.entity.tangle.TangleWidgetFactory;
+import view.event.NodeListener;
 import view.field.FieldWidget;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
 
-public class GamePanel extends JFrame implements GameStateListener {
+public class GamePanel extends JFrame implements GameStateListener, NodeListener {
 
   private final Model model;
   private final FieldWidget fieldWidget;
+  boolean isTangled;
+
+  private final String MESSAGE_TITLE = "Игра окончена";
+  private final String SUCCESSFUL_END_MESSAGE = "Поздравляем! Вы распутали этот клубок :)";
 
   public GamePanel(Model model, FieldWidget field) {
     this.model = model;
     this.fieldWidget = field;
 
-    fieldWidget.setTangle(TangleWidgetFactory.getTangle(model.getTangle()));
+    TangleWidget tangleWidget = TangleWidgetFactory.getTangle(model.getTangle());
+    fieldWidget.setTangle(tangleWidget);
+
     model.addListener(this);
+    subscribeOnNodes(tangleWidget);
 
     // Фиксированные размеры главного окна
     setResizable(false);
@@ -34,13 +48,14 @@ public class GamePanel extends JFrame implements GameStateListener {
     setTitle("Запутанная схема");
   }
 
+  private void subscribeOnNodes(TangleWidget tangle) {
+    for(NodeWidget node : tangle.getNodes())
+      node.addListener(this);
+  }
+
   @Override
   public void tangleUpdated(boolean isTangled) {
+    this.isTangled = isTangled;
     fieldWidget.repaint();
-    final String MESSAGE_TITLE = "Игра окончена";
-
-    // Пользователь распутал клубок
-    if(!isTangled)
-      JOptionPane.showMessageDialog(this, "Поздравляем! Вы распутали этот клубок :)", MESSAGE_TITLE, JOptionPane.PLAIN_MESSAGE);
   }
 }
