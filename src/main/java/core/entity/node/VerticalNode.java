b1b249e07b7dea99cf89fb2633d91b18d7807d3d
nikitakalnov <jdmcorolla21@gmail.com>
2020-11-08 13:01:38 +0400

    Добавлен конструктор, принимающий точку

--- src/main/java/core/entity/node/VerticalNode.java
+++ src/main/java/core/entity/node/VerticalNode.java
@@ -1,8 +1,14 @@
 package core.entity.node;
 
+import core.field.geometry.Point;
+
 public class VerticalNode extends Node {
   public VerticalNode(int x, int y) {
     super(x, y);
+  }
+
+  public VerticalNode(Point position) {
+    super(position.getX(), position.getY());
   }
 
   @Override
